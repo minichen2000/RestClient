aef1d7bd37497329547aa28a6ff799c949ea4cf7
Chen Min <min.e.chen@alcatel-sbell.com.cn>
2017-04-17 10:24:09 +0800

    ..

--- js/main/main-controller.js
+++ js/main/main-controller.js
@@ -12,27 +12,13 @@
     function MainController($http, $scope, commonUtil, logger, $interval, $timeout, serverNotificationService) {
         var vm = this;
         vm.requestProcessing=false;
-        vm.logining=false;
-        vm.logouting=false;
-        vm.connected=false;
-
-        vm.otnIP='135.251.96.33';
-        vm.otnPort=8443;
-        vm.casIP='135.251.96.33';
-        vm.casPort=8443;
-        vm.casUrl='/cas/v1/tickets';
-        vm.presentationIP='135.251.96.34';
-        vm.guiUsername='alcatel';
-        vm.guiPassword='Lucent2.@';
-        vm.omsUrl='/oms1350/data/plat/session/login';
-
         vm.notifications=[];
         vm.autoScroll=true;
         vm.onLoad = function (instance) {
             //instance.expandAll();
         };
 
-        vm.baseUrl='http://135.251.103.125:9000/Adapter';
+        vm.baseUrl='http://localhost:9000/Adapter';
         vm.path="/nes";
         vm.result=null;
         vm.postBody=null;
@@ -40,99 +26,23 @@
         vm.resultOptions={mode: 'code'};
 
 
-        vm.wsAddress="ws://135.251.103.125:19000";
+        vm.wsAddress="ws://localhost:19000";
+        vm.wsAddressHistory=new HashMap();
 
         vm.ws_state=2;
 
-        vm.onLogin=function(){
-            vm.logining=true;
-                $http({
-                    method: 'post',
-                    url: './login',
-                    //url:'http://www.mobisoftwarestudio.com',
-                    //url:'https://api.shanbay.com/bdc/search/',
-                    //url:'http://127.0.0.1:8080/',
-                    //headers:{'Content-Type': 'application/json, text/plain, */*', 'Access-Control-Allow-Origin':'*'},
-                    params: {
-                        'otnip': vm.otnIP,
-                        'otnport': vm.otnPort,
-                        'casip': vm.casIP,
-                        'casport': vm.casPort,
-                        'casurl': vm.casUrl,
-                        'presentationip': vm.presentationIP,
-                        'guiusername': vm.guiUsername,
-                        'guipassword': vm.guiPassword,
-                        'omsurl': vm.omsUrl
-                    }
-                })
-                .then(function(rsp){
-                    logger.debug("rsp:"+JSON.stringify(rsp, null, 2));
-                    //window.alert("OK");
-                })
-                .catch(function(rsp){
-                    var error=JSON.stringify(rsp, null, 2);
-                    logger.error("rsp:"+error);
-                    //window.alert("Error:\n"+error);
-                });
+        function addContentToHistory(content, history){
+            history.set(content, content);
         }
-
-        vm.onLogout=function(){
-            vm.logouting=true;
-            $http({
-                method: 'post',
-                url: './logout'
-            })
-                .then(function(rsp){
-                    logger.debug("rsp:"+JSON.stringify(rsp, null, 2));
-                    //window.alert("OK");
-                })
-                .catch(function(rsp){
-                    var error=JSON.stringify(rsp, null, 2);
-                    logger.error("rsp:"+error);
-                    //window.alert("Error:\n"+error);
-                });
-        }
-
-
-        function checkStatus(){
-            $http({
-                method: 'post',
-                url: './status'
-            })
-                .then(function(rsp){
-                    //logger.debug("rsp:"+JSON.stringify(rsp, null, 2));
-                    if(rsp.data.status.toLowerCase()=='connected'){
-                        vm.connected=true;
-                        vm.logining=false;
-                    }else{
-                        vm.connected=false;
-                        vm.logouting=false;
-                    }
-
-                    $timeout(checkStatus, 1000);
-                })
-                .catch(function(rsp){
-                    var error=JSON.stringify(rsp, null, 2);
-                    //logger.error("rsp:"+error);
-                    $timeout(checkStatus, 1000);
-                });
-        }
-
-
-        //checkStatus();
-
-
-
-
 
 
         vm.postBodyModeSwith=function(){
             vm.postBodyOptions.mode=vm.postBodyOptions.mode=='code' ? 'tree' : 'code';
-        }
+        };
 
         vm.resultModeSwith=function(){
             vm.resultOptions.mode=vm.resultOptions.mode=='code' ? 'tree' : 'code';
-        }
+        };
 
         function onRequest(method_){
             vm.requestProcessing=true;
@@ -158,30 +68,34 @@
         }
         vm.onGet=function(){
             onRequest('get');
-        }
+        };
         vm.onPost=function(){
             onRequest('post');
-        }
+        };
         vm.onPut=function(){
             onRequest('put');
-        }
+        };
         vm.onPatch=function(){
             onRequest('patch');
-        }
+        };
         vm.onDelete=function(){
             onRequest('delete');
-        }
+        };
 
+        vm.onWsAddressHistorySelected=function(address){
+            vm.wsAddress=address;
+        };
         vm.onNotificationConnect=function(){
             serverNotificationService.connect(vm.wsAddress, "5000");
-        }
+            logger.debug("vm.wsAddressHistory:\n"+JSON.stringify(vm.wsAddressHistory.keys(), null,2));
+        };
         vm.onNotificationDisconnect=function(){
             serverNotificationService.close();
-        }
+        };
 
         vm.clearNotifications=function(){
             vm.notifications.length=0;
-        }
+        };
 
         function tryToAutoScroll(){
             if(vm.autoScroll){
@@ -193,7 +107,8 @@
         vm.swithAutoScroll=function(){
             vm.autoScroll=!vm.autoScroll;
             tryToAutoScroll();
-        }
+        };
+
 
 
 
@@ -216,6 +131,7 @@
                         vm.notifications.push({
                             line1: '['+commonUtil.formatNowDateTime()+']  WebSocket connected.'
                         });
+                        addContentToHistory(vm.wsAddress, vm.wsAddressHistory);
                     }else if(0==vm.ws_state){
                         vm.notifications.push({
                             line0: '['+commonUtil.formatNowDateTime()+']  WebSocket error.'
